d976d1d730880c54fd20c018b1455b1fb516261f
dagbolaurinda-design <dagbolaurinda@gmail.com>
2026-03-20 08:22:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -550,7 +550,7 @@ app.layout = html.Div(
                             style={
                                 "width": "44%",
                                 "height": "380px",
-                                "marginTop": "-135px"
+                                "marginTop": "-110px"
                             }
                         ),
 
